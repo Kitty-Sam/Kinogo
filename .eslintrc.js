d9891a309399373c4959dfbb7c1619203aef6e93
kittypitty <samutic40@gmail.com>
2023-02-16 13:10:37 +0300

    add eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,31 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-      },
+    root: true,
+    extends: ['@react-native-community', 'airbnb', 'airbnb-typescript', 'airbnb/hooks', 'prettier'],
+
+    parser: '@typescript-eslint/parser',
+
+    ignorePatterns: ['.eslintrc.js', 'metro.config.js'],
+
+    parserOptions: {
+        project: ['./tsconfig.json'],
+        sourceType: 'module',
     },
-  ],
+
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                '@typescript-eslint/no-shadow': ['error'],
+                'no-shadow': 'off',
+                'no-undef': 'off',
+                'simple-import-sort/imports': 'error',
+                'simple-import-sort/exports': 'error',
+                'import/first': 'error',
+                'import/newline-after-import': 'error',
+                'import/no-duplicates': 'error',
+            },
+        },
+    ],
+
+    plugins: ['@typescript-eslint', 'simple-import-sort'],
 };
