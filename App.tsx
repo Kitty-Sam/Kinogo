c17e0976b9001e9fd3419ccad86efa4b2aecfabb
kittypitty <samutic40@gmail.com>
2023-02-21 21:12:08 +0300

    add layout for welcome screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,6 @@ import { AuthStack } from '~navigation/AuthStack';
 import { NavigationContainer } from '@react-navigation/native';
 
 export const App = () => {
-    const [isLogged, setIsLogged] = useState(true);
+    const [isLogged, setIsLogged] = useState(false);
     return <NavigationContainer>{isLogged ? <RootStack /> : <AuthStack />}</NavigationContainer>;
 };
--- src/screens/WelcomeScreen/index.tsx
+++ src/screens/WelcomeScreen/index.tsx
@@ -1,12 +1,75 @@
 import React, { FC } from 'react';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 import { WelcomeTabScreenProps } from '~screens/WelcomeScreen/type';
+import {
+    LinkText,
+    Logo,
+    ScreenContainer,
+    StudiosContainer,
+    Text,
+    TextContainer,
+    Title,
+} from '~screens/WelcomeScreen/style';
+import { Button } from '~components/Button';
+import { Image, TouchableOpacity } from 'react-native';
+
+const buttons = [
+    {
+        icon: require('~assets/icons/person.png'),
+        title: 'Create an Account',
+        onPress: () => {},
+        bgColor: '#404040',
+        color: '#FFFFFF',
+    },
+    {
+        icon: require('~assets/icons/google.png'),
+        title: 'Continue with google',
+        onPress: () => {},
+        bgColor: '#FFFFFF',
+        color: '#808080',
+    },
+    {
+        icon: require('~assets/icons/fb.png'),
+        title: 'Sign up with Facebook',
+        onPress: () => {},
+        bgColor: '#1877F2',
+        color: '#FFFFFF',
+    },
+    {
+        icon: require('~assets/icons/github.png'),
+        title: 'Sign up with Github',
+        onPress: () => {},
+        bgColor: '#000000',
+        color: '#FFFFFF',
+    },
+];
+
+const studios = [
+    { icon: require('~assets/icons/marvel.png') },
+    { icon: require('~assets/icons/DC.png') },
+    { icon: require('~assets/icons/warner.png') },
+    { icon: require('~assets/icons/netflix.png') },
+];
 
 export const WelcomeScreen: FC<WelcomeTabScreenProps> = () => {
     return (
-        <SafeAreaView>
-            <Text>Welcome</Text>
-        </SafeAreaView>
+        <ScreenContainer bgColor={'#1e1f27'}>
+            <Logo source={require('~assets/icons/logo.png')} />
+            <Title textColor={'#fff'}>Great Movies in the best cinema! We care about your comfort.</Title>
+            {buttons.map(({ title, onPress, icon, bgColor, color }) => (
+                <Button title={title} onPress={onPress} icon={icon} key={title} bgColor={bgColor} textColor={color} />
+            ))}
+            <TextContainer>
+                <Text textColor={'#fff'}>Already has an account?</Text>
+                <TouchableOpacity onPress={() => console.log('press login')}>
+                    <LinkText textColor={'#fff'}> Login please.</LinkText>
+                </TouchableOpacity>
+            </TextContainer>
+            <StudiosContainer>
+                {studios.map((studio) => (
+                    <Image key={studio.icon} source={studio.icon} />
+                ))}
+            </StudiosContainer>
+            <Text textColor={'#fff'}>2023 Version 0.0.1</Text>
+        </ScreenContainer>
     );
 };
