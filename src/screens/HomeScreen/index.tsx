398de8ab8ac4e146908ea905a6b01aaefd2f8be1
kittypitty <samutic40@gmail.com>
2023-02-22 15:52:20 +0300

    add layout for HomePage

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,12 +1,74 @@
-import React, { FC } from 'react';
-import { Text } from 'react-native';
+import React, { FC, useState } from 'react';
+import { FlatList } from 'react-native';
 import { HomeTabScreenProps } from '~screens/HomeScreen/type';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+    CategoriesListContainer,
+    CategoryFilmText,
+    CategoryFilmTextContainer,
+    ChapterTitleText,
+    FilmContainer,
+    FilmImage,
+    FilmTitleText,
+    PlayerContainer,
+    PlayerImage,
+    ScreenContainer,
+} from '~screens/HomeScreen/style';
+
+const categories = ['Action', 'Comedy', 'Romance', 'Thriller', 'Fantasy'];
+
+const slideList = Array.from({ length: 5 }).map((_, i) => {
+    return {
+        id: i.toString(),
+        image: `https://picsum.photos/1440/2842?random=${i}`,
+    };
+});
 
 export const HomeScreen: FC<HomeTabScreenProps> = () => {
+    const [category, setCategory] = useState('');
+
+    const renderItem = ({ item }: { item: { id: string; image: string } }) => {
+        return (
+            <FilmContainer>
+                <FilmImage source={{ uri: item.image }} />
+                <FilmTitleText textColor={'#fff'}>Film title</FilmTitleText>
+            </FilmContainer>
+        );
+    };
+
     return (
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>
+        <ScreenContainer bgColor={'#1e1f27'}>
+            <ChapterTitleText textColor={'#fff'}>Coming Soon</ChapterTitleText>
+
+            <PlayerContainer>
+                <PlayerImage source={{ uri: 'https://picsum.photos/1440/2842?random=7' }} />
+            </PlayerContainer>
+
+            <CategoriesListContainer>
+                <FlatList
+                    contentContainerStyle={{ flex: 1, justifyContent: 'space-around' }}
+                    horizontal={true}
+                    data={categories}
+                    renderItem={({ item }) => (
+                        <CategoryFilmTextContainer
+                            bgColor={category === item ? '#D98639' : '#1e1f27'}
+                            onPress={() => setCategory(item)}
+                        >
+                            <CategoryFilmText textColor={'#fff'}>{item}</CategoryFilmText>
+                        </CategoryFilmTextContainer>
+                    )}
+                />
+            </CategoriesListContainer>
+
+            <ChapterTitleText textColor={'#fff'}>Now Showing</ChapterTitleText>
+
+            <FlatList
+                data={slideList}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                pagingEnabled={true}
+            />
+        </ScreenContainer>
     );
 };
